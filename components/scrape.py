89929939ce2869b08b68475038784a317331d422
saksham <sakshamgupta0155@gmail.com>
2026-05-31 02:44:19 +0530

    Delete components/scrape.py

--- components/scrape.py
+++
@@ -1,8 +0,0 @@
-from playwright.sync_api import sync_playwright
-
-with sync_playwright() as p:
-    browser = p.chromium.launch()
-    page = browser.new_page()
-    page.goto("https://langchain-ai.github.io/langgraph/tutorials/introduction/")
-    print(page.content())
-    browser.close()
